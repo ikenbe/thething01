3325d2f297055d6bc3c6962b5a550b9ad22cd24a
Ken Leung <ikenbe@users.noreply.github.com>
2014-09-13 17:45:48 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 var main = function(){
+
+//initial
 	$('span').each(function(){
 		$(this).html(Math.floor(Math.random()*4));
 	});
 
-		var $r1c1 = $('.r1.c1'), $r1c2 = $('.r1.c2'), $r1c3 = $('.r1.c3'), $r1c4 = $('.r1.c4');
-		var $r2c1 = $('.r2.c1'), $r2c2 = $('.r2.c2'), $r2c3 = $('.r2.c3'), $r2c4 = $('.r2.c4');
-		var $r3c1 = $('.r3.c1'), $r3c2 = $('.r3.c2'), $r3c3 = $('.r3.c3'), $r3c4 = $('.r3.c4');
-		var $r4c1 = $('.r4.c1'), $r4c2 = $('.r4.c2'), $r1c3 = $('.r4.c3'), $r1c4 = $('.r4.c4');
+//The moving method from A to B
 		var moveIt = function(cellA,cellB){
-
+//debug
 			console.log($(cellA).html()+","+$(cellB).html());
-			console.log($('cellA.passed').text());
-			if($('cellA.passed')=== true){console.log("STOPPED!")}
+			console.log(typeof $('.passed').filter(cellA).val());
+			console.log($('.passed').find(cellA));
+
+
+//pan4duan4
+			if($('span.passed').filter(cellA).val()!== undefined){console.log("STOPPED!"+$('span.passed').filter(cellA).val())}
 			else if(($(cellA).html()=== $(cellB).html())&&(parseInt($(cellA).html())>=3)){
 				console.log('a');
+				$(cellB).addClass('passed');
 				$(cellA).html(parseInt($(cellA).html())+parseInt($(cellB).html()));}
 			else if(parseInt($(cellA).html())===0){
 				console.log('b');
@@ -24,12 +28,15 @@ var main = function(){
 				$(cellA).text("3");
 				$(cellB).addClass('passed');}			
 		}
-
+//Key Pressing Monitor
 	$(document).keydown(function(key){
-		$('.passed').removeClass('passed');
+//Remove status
+		$('span').removeClass('passed');
+//Define the next number
 		var $next = Math.floor(Math.random()*3+1);
 		if($next===3){for(var i=0;i<0.3;i=Math.random()){$next=$next*2}};
 		$('#next').text($next);
+//
 		console.log(key.which);
 		switch(key.which){
 //move left
